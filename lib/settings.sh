2632e6ccda160069b1ec7de69e1b14c82842f0f9
Martin Ritchie <ritchiem@apache.org>
2017-06-18 23:45:40 -0400

    Create dev, core and personal settings

--- lib/settings.sh
+++ lib/settings.sh
@@ -11,14 +11,14 @@ set -o pipefail
 IFS=$'\n\t'
 
 # Ruby
-export MRI=2.4.0
+export MRI=2.3.1
 
 # Repositories
-export REPO_RUBY_SETUP=v7.0.0
-export REPO_GO_SETUP=v2.1.0
-export REPO_NPM_SETUP=v2.1.0
-export REPO_SUBLIME_TEXT_SETUP=v6.0.0
-export REPO_DOTFILES=v24.2.0
+export REPO_RUBY_SETUP=v6.0.0
+export REPO_GO_SETUP=v2.0.0
+export REPO_NPM_SETUP=v2.0.0
+export REPO_SUBLIME_TEXT_SETUP=v5.0.0
+export REPO_DOTFILES=v23.0.0
 
 # Applications
 export ALFRED_APP_NAME="Alfred 3.app"
@@ -32,7 +32,8 @@ export BARTENDER_APP_NAME="Bartender 2.app"
 export BARTENDER_APP_URL="https://www.macbartender.com/Demo/Bartender%202.zip"
 
 export CARBON_COPY_CLONER_APP_NAME="Carbon Copy Cloner.app"
-export CARBON_COPY_CLONER_APP_URL="https://bombich.scdn1.secure.raxcdn.com/software/files/ccc-4.1.12.4485.zip"
+#export CARBON_COPY_CLONER_APP_URL="https://bombich.scdn1.secure.raxcdn.com/software/files/ccc-4.1.12.4485.zip"
+export CARBON_COPY_CLONER_APP_URL="https://bombich.com/software/download_ccc.php?v=latest"
 
 export CHEATSHEET_APP_NAME=CheatSheet.app
 export CHEATSHEET_APP_URL="https://www.cheatsheetapp.com/CheatSheet/download.php"
@@ -40,6 +41,10 @@ export CHEATSHEET_APP_URL="https://www.cheatsheetapp.com/CheatSheet/download.php
 export CHROME_APP_NAME="Google Chrome.app"
 export CHROME_VOLUME_NAME="Google Chrome"
 export CHROME_APP_URL="https://dl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
+
+export CHROME_CANARY_APP_NAME="Google Chrome Canary.app"
+export CHROME_CANARY_VOLUME_NAME="Google Chrome Canary"
+export CHROME_CANARY_APP_URL="https://dl.google.com/release2/q/canary/googlechrome.dmg"
 
 export CLOAK_APP_NAME=Cloak.app
 export CLOAK_VOLUME_NAME="Cloak"
@@ -58,11 +63,11 @@ export DROPBOX_APP_URL="https://www.dropbox.com/download?src=index&plat=mac"
 
 export FIREFOX_APP_NAME=Firefox.app
 export FIREFOX_VOLUME_NAME="Firefox"
-export FIREFOX_APP_URL="https://download.mozilla.org/?product=firefox-50.1.0-SSL&os=osx&lang=en-US"
+export FIREFOX_APP_URL="https://download.mozilla.org/?product=firefox-49.0.1-SSL&os=osx&lang=en-US"
 
 export HAND_BRAKE_APP_NAME="HandBrake.app"
-export HAND_BRAKE_VOLUME_NAME="HandBrake-1.0.1"
-export HAND_BRAKE_APP_URL="https://handbrake.fr/mirror/HandBrake-1.0.1.dmg"
+export HAND_BRAKE_VOLUME_NAME="HandBrake-1.0.2"
+export HAND_BRAKE_APP_URL="http://handbrake.fr/rotation.php?file=HandBrake-1.0.2.dmg"
 
 export HAZEL_APP_NAME="Install Hazel.app"
 export HAZEL_VOLUME_NAME="Hazel"
@@ -90,9 +95,13 @@ export MARP_APP_URL="https://github.com/yhatt/marp/releases/download/v0.0.10/0.0
 export MICRO_SNITCH_APP_NAME="Micro Snitch.app"
 export MICRO_SNITCH_APP_URL="https://www.obdev.at/downloads/MicroSnitch/MicroSnitch-1.2.zip"
 
-export OMNIGRAFFLE_APP_NAME="OmniGraffle Professional 5.app"
-export OMNIGRAFFLE_VOLUME_NAME="OmniGrafflePro"
-export OMNIGRAFFLE_APP_URL="http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniGrafflePro-5.4.4.dmg"
+export OMNIGRAFFLE_APP_NAME="OmniGraffle 5.app"
+export OMNIGRAFFLE_VOLUME_NAME="OmniGraffle"
+export OMNIGRAFFLE_APP_URL="http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniGraffle-5.4.4.dmg"
+
+export OMNIGRAFFLEPRO_APP_NAME="OmniGraffle Professional 5.app"
+export OMNIGRAFFLEPRO_VOLUME_NAME="OmniGrafflePro"
+export OMNIGRAFFLEPRO_APP_URL="http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniGrafflePro-5.4.4.dmg"
 
 export OPEN_OFFICE_APP_NAME=OpenOffice.app
 export OPEN_OFFICE_VOLUME_NAME="OpenOffice"
@@ -159,105 +168,96 @@ export VLC_APP_NAME=VLC.app
 export VLC_VOLUME_NAME="vlc-2.2.4"
 export VLC_APP_URL="https://get.videolan.org/vlc/2.2.4/macosx/vlc-2.2.4.dmg"
 
-export SUBLIME_TEXT_EXTENSION_ROOT="$HOME/Library/Application Support/Sublime Text 3/Packages"
-export ADVANCED_NEW_FILE_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/AdvancedNewFile"
-export ADVANCED_NEW_FILE_EXTENSION_URL="https://github.com/skuroda/Sublime-AdvancedNewFile.git"
-export ALIGNMENT_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Alignment"
-export ALIGNMENT_EXTENSION_URL="https://github.com/wbond/sublime_alignment.git"
-export APPLY_SYNTAX_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/ApplySyntax"
-export APPLY_SYNTAX_EXTENSION_URL="https://github.com/facelessuser/ApplySyntax.git"
-export AUTOFILENAME_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/AutoFileName"
-export AUTOFILENAME_EXTENSION_URL="https://github.com/BoundInCode/AutoFileName.git"
-export AUTOFILENAME_EXTENSION_OPTIONS="--branch st3"
-export AUTOPREFIXER_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Autoprefixer"
-export AUTOPREFIXER_EXTENSION_URL="https://github.com/sindresorhus/sublime-autoprefixer.git"
-export BRACKET_HIGHLIGHTER_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/BracketHighlighter"
-export BRACKET_HIGHLIGHTER_EXTENSION_URL="https://github.com/facelessuser/BracketHighlighter.git"
-export CHANGE_QUOTES_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/SublimeChangeQuotes"
-export CHANGE_QUOTES_EXTENSION_URL="https://github.com/colinta/SublimeChangeQuotes.git"
-export CHANGE_QUOTES_EXTENSION_OPTIONS="--branch st2"
-export COLOR_HIGHLIGHTER_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Color Highlighter"
-export COLOR_HIGHLIGHTER_EXTENSION_URL="https://github.com/Monnoroch/ColorHighlighter.git"
-export CSSCOMB_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/CSScomb"
-export CSSCOMB_EXTENSION_URL="https://github.com/csscomb/csscomb-for-sublime.git"
-export CTAGS_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/CTags"
-export CTAGS_EXTENSION_URL="https://github.com/SublimeText/CTags.git"
-export DASH_DOC_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/DashDoc"
-export DASH_DOC_EXTENSION_URL="https://github.com/farcaller/DashDoc.git"
-export EASY_MOTION_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/EasyMotion"
-export EASY_MOTION_EXTENSION_URL="https://github.com/tednaleid/sublime-EasyMotion.git"
-export EASY_MOTION_EXTENSION_OPTIONS="--branch st3"
-export ELM_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Elm"
-export ELM_EXTENSION_URL="https://github.com/elm-community/Elm.tmLanguage.git"
-export EMMET_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Emmet"
-export EMMET_EXTENSION_URL="https://github.com/sergeche/emmet-sublime.git"
-export EMMET_LIVE_STYLE_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/LiveStyle"
-export EMMET_LIVE_STYLE_EXTENSION_URL="https://github.com/livestyle/sublime-text.git"
-export GIST_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Gist"
-export GIST_EXTENSION_URL="https://github.com/condemil/Gist.git"
-export GIT_GUTTER_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/GitGutter"
-export GIT_GUTTER_EXTENSION_URL="https://github.com/jisaacks/GitGutter.git"
-export GUTTER_COLOR_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Gutter Color"
-export GUTTER_COLOR_EXTENSION_URL="https://github.com/ggordan/GutterColor.git"
-export HTML_PRETTIFY_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Sublime-HTMLPrettify"
-export HTML_PRETTIFY_EXTENSION_URL="https://github.com/victorporof/Sublime-HTMLPrettify.git"
-export JSCS_FORMATTER_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/JSCS-Formatter"
-export JSCS_FORMATTER_EXTENSION_URL="https://github.com/TheSavior/SublimeJSCSFormatter.git"
-export JSCS_LINTER_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/SublimeLinter-jscs"
-export JSCS_LINTER_EXTENSION_URL="https://github.com/SublimeLinter/SublimeLinter-jscs.git"
-export KEYMAPS_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Keymaps"
-export KEYMAPS_EXTENSION_URL="https://github.com/MiroHibler/sublime-keymaps.git"
-export LINTER_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/SublimeLinter"
-export LINTER_EXTENSION_URL="https://github.com/SublimeLinter/SublimeLinter3.git"
-export LOCAL_HISTORY_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Local History"
-export LOCAL_HISTORY_EXTENSION_URL="https://github.com/vishr/local-history.git"
-export MARKDOWN_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/sublime-markdown-extended"
-export MARKDOWN_EXTENSION_URL="https://github.com/jonschlinkert/sublime-markdown-extended.git"
-export MOVE_TAB_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/MoveTab"
-export MOVE_TAB_EXTENSION_URL="https://github.com/SublimeText/MoveTab.git"
-export REEK_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/SublimeLinter-contrib-reek"
-export REEK_EXTENSION_URL="https://github.com/codequest-eu/SublimeLinter-contrib-reek.git"
-export RUBOCOP_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/RuboCop"
-export RUBOCOP_EXTENSION_URL="https://github.com/pderichs/sublime_rubocop.git"
-export RUBY_EXTRACT_METHOD_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/RubyExtractMethod"
-export RUBY_EXTRACT_METHOD_EXTENSION_URL="https://github.com/pashamur/ruby-extract-method.git"
-export RUBY_SLIM_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Ruby-Slim.tmbundle"
-export RUBY_SLIM_EXTENSION_URL="https://github.com/slim-template/ruby-slim.tmbundle.git"
-export SCSS_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/SCSS"
-export SCSS_EXTENSION_URL="https://github.com/MarioRicalde/SCSS.tmbundle.git"
-export SCSS_LINT_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/SublimeLinter-scss-lint"
-export SCSS_LINT_EXTENSION_URL="https://github.com/attenzione/SublimeLinter-scss-lint.git"
-export SHELLCHECK_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/SublimeLinter-shellcheck"
-export SHELLCHECK_EXTENSION_URL="https://github.com/SublimeLinter/SublimeLinter-shellcheck.git"
-export SIDEBAR_ENHANCEMENTS_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/SideBarEnhancements"
-export SIDEBAR_ENHANCEMENTS_EXTENSION_URL="https://github.com/titoBouzout/SideBarEnhancements.git"
-export TABLE_EDITOR_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Table Editor"
-export TABLE_EDITOR_EXTENSION_URL="https://github.com/vkocubinsky/SublimeTableEditor.git"
-export TRAILING_SPACES_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/TrailingSpaces"
-export TRAILING_SPACES_EXTENSION_URL="https://github.com/SublimeText/TrailingSpaces.git"
-export WEB_INSPECTOR_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/Web Inspector"
-export WEB_INSPECTOR_EXTENSION_URL="https://github.com/sokolovstas/SublimeWebInspector.git"
-export WRAP_PLUS_EXTENSION_PATH="$SUBLIME_TEXT_EXTENSION_ROOT/WrapPlus"
-export WRAP_PLUS_EXTENSION_URL="https://github.com/ehuss/Sublime-Wrap-Plus.git"
+export DUETDISPLAY_APP_NAME=duet.app
+export DUETDISPLAY_APP_URL="https://www.duetdisplay.com/mac"
 
-export VIM_EXTENSION_ROOT="$HOME/.vim/bundle"
-export VIM_BUNDLER_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-bundler"
-export VIM_BUNDLER_EXTENSION_URL="https://github.com/tpope/vim-bundler"
-export VIM_COMMENTARY_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-commentary"
-export VIM_COMMENTARY_EXTENSION_URL="https://github.com/tpope/vim-commentary.git"
-export VIM_FUGITIVE_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-fugitive"
-export VIM_FUGITIVE_EXTENSION_URL="https://github.com/tpope/vim-fugitive.git"
-export VIM_GIT_GUTTER_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-gitgutter"
-export VIM_GIT_GUTTER_EXTENSION_URL="https://github.com/airblade/vim-gitgutter.git"
-export VIM_PATHOGEN_EXTENSION_PATH="$HOME/.vim/autoload/pathogen.vim"
-export VIM_PATHOGEN_EXTENSION_URL="https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
-export VIM_RAILS_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-rails"
-export VIM_RAILS_EXTENSION_URL="https://github.com/tpope/vim-rails.git"
-export VIM_RUBY_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-ruby"
-export VIM_RUBY_EXTENSION_URL="https://github.com/vim-ruby/vim-ruby.git"
-export VIM_TEXT_OBJECT_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-textobj-user"
-export VIM_TEXT_OBJECT_EXTENSION_URL="https://github.com/kana/vim-textobj-user.git"
-export VIM_TEXT_OBJECT_RUBY_BLOCK_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-textobj-rubyblock"
-export VIM_TEXT_OBJECT_RUBY_BLOCK_EXTENSION_URL="https://github.com/nelstrom/vim-textobj-rubyblock.git"
-export VIM_UNIMPAIRED_EXTENSION_PATH="$VIM_EXTENSION_ROOT/vim-unimpaired"
-export VIM_UNIMPAIRED_EXTENSION_URL="https://github.com/tpope/vim-unimpaired.git"
+export TEXTMATE_APP_NAME=TextMate.app
+export TEXTMATE_APP_URL="https://api.textmate.org/downloads/release?os=10.8"
+
+export IDEA_APP_NAME="IntelliJ IDEA.app"
+export IDEA_VOLUME_NAME="IntelliJ IDEA"
+export IDEA_APP_URL="https://download.jetbrains.com/idea/ideaIU-2016.3.4.dmg"
+
+export PYCHARM_APP_NAME=PyCharm.app
+export PYCHARM_VOLUME_NAME=PyCharm
+export PYCHARM_APP_URL="https://download.jetbrains.com/python/pycharm-professional-2016.3.1.dmg"
+
+export DATAGRIP_APP_NAME=DataGrip.app
+export DATAGRIP_VOLUME_NAME=DataGrip
+export DATAGRIP_APP_URL="https://download.jetbrains.com/datagrip/datagrip-2016.3.1.dmg"
+
+export WEBSTORM_APP_NAME="WebStorm.app"
+export WEBSTORM_VOLUME_NAME=WebStorm
+export WEBSTORM_APP_URL="https://download.jetbrains.com/webstorm/WebStorm-2016.3.2.dmg"
+
+export PHPSTORM_APP_NAME=PhpStorm.app
+export PHPSTORM_VOLUME_NAME=PhpStorm
+export PHPSTORM_APP_URL="https://download.jetbrains.com/webide/PhpStorm-2016.3.2.dmg"
+
+export DOCKER_APP_NAME=Docker.app
+export DOCKER_VOLUME_NAME=Docker
+export DOCKER_APP_URL="https://download.docker.com/mac/stable/Docker.dmg"
+
+export TRANSMISSION_APP_NAME=Transmission.app
+export TRANSMISSION_VOLUME_NAME=Transmission
+export TRANSMISSION_APP_URL="https://github.com/transmission/transmission-releases/raw/master/Transmission-2.92.dmg"
+
+export TUNNELBLICK_APP_NAME=Tunnelblick.app
+export TUNNELBLICK_VOLUME_NAME=Tunnelblick
+export TUNNELBLICK_APP_URL="https://tunnelblick.net/release/Tunnelblick_3.7.0_build_4790.dmg"
+
+export BOXER_APP_NAME="Boxer.app"
+export BOXER_APP_URL="http://boxerapp.com/download/latest"
+
+export AUDIO_HIJACK_APP_NAME="Audio Hijack Pro.app"
+export AUDIO_HIJACK_APP_URL="https://rogueamoeba.com/legacy/downloads/AudioHijackPro-2116.zip"
+
+export CHICKEN_APP_NAME="Chicken.app"
+export CHICKEN_VOLUME_NAME="Chicken"
+export CHICKEN_APP_URL="https://downloads.sourceforge.net/project/chicken/Chicken-2.2b2.dmg"
+
+export GARAGE_SALE_APP_NAME="GarageSale.app"
+export GARAGE_SALE_VOLUME_NAME="GarageSale 7.0.6"
+export GARAGE_SALE_APP_URL="https://downloads.iwascoding.com/downloads/GarageSale_7.0.6.dmg"
+
+export GITHUB_DESKTOP_APP_NAME="GitHub Desktop.app"
+export GITHUB_DESKTOP_APP_URL="https://central.github.com/mac/latest"
+
+export LITTLE_SNITCH_INSTALLER_NAME="Little Snitch Installer.app"
+export LITTLE_SNITCH_APP_NAME="Little Snitch Configuration.app"
+export LITTLE_SNITCH_VOLUME_NAME="Little Snitch 3.7.3"
+export LITTLE_SNITCH_APP_URL="https://www.obdev.at/downloads/littlesnitch/LittleSnitch-3.7.3.dmg"
+
+export AMAZON_MUSIC_INSTALLER_NAME="Amazon Music Installer.app"
+export AMAZON_MUSIC_APP_NAME="Amazon Music.app"
+export AMAZON_MUSIC_VOLUME_NAME="Amazon Music"
+export AMAZON_MUSIC_APP_URL="https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/20170131/07195565c1/AmazonMusicInstaller.dmg"
+
+export GRAPHIC_CONVERTER_APP_NAME="GraphicConverter 10.app"
+export GRAPHIC_CONVERTER_VOLUME_NAME="GraphicConverter 10"
+export GRAPHIC_CONVERTER_APP_URL="https://www.lemkesoft.info/files/graphicconverter/gc10_build2691.dmg"
+
+export DOCKER_TOOLBOX_NAME="Docker Toolbox"
+export DOCKER_TOOLBAX_APP_NAME="Docker/Kitematic (Beta).app"
+export DOCKER_TOOLBOX_PKG_URL="https://download.docker.com/mac/stable/DockerToolbox.pkg"
+
+export DOCKER_CE_APP_NAME="Docker.app"
+export DOCKER_CE_VOLUME_NAME="Docker"
+export DOCKER_CE_APP_URL="https://download.docker.com/mac/stable/Docker.dmg"
+
+export DOCKER_CE_BETA_APP_NAME="Docker.app"
+export DOCKER_CE_BETA_VOLUME_NAME="Docker"
+export DOCKER_CE_BETA_APP_URL="https://download.docker.com/mac/beta/Docker.dmg"
+
+export PASSWORD_WALLET_APP_NAME="PasswordWallet.app"
+export PASSWORD_WALLET_VOLUME_NAME="PasswordWallet 4 for OS X"
+export PASSWORD_WALLET_APP_URL="http://www.selznick1.com/download/PasswordWallet_4_8_5_Mac.dmg"
+
+export BLOCK_BLOCK_INSTALL_COMMAND="BlockBlock_Installer.app/Contents/MacOS/BlockBlock -install"
+export BLOCK_BLOCK_INSTALL_CHECK_URL="/Library/Objective-See/BlockBlock/BlockBlock.app"
+export BLOCK_BLOCK_ZIP_URL="https://bitbucket.org/objective-see/deploy/downloads/BlockBlock_0.9.7.zip"
+
+export LITTLEFLOCKER_APP_NAME="Little Flocker Configuration.app"
+export LITTLEFLOCKER_PKG_INSTALLER_NAME="Install Little Flocker.pkg"
+export LITTLEFLOCKER_DMG_VOLUME_NAME="Little Flocker"
+export LITTLEFLOCKER_DMG_URL="https://www.littleflocker.com/downloads/LittleFlocker-1.5.0.dmg"
